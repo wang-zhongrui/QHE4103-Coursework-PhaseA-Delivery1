e02a70bc4f85718d71351d71c4f291b4287f1d05
Zhongrui Wang <wang.zhongrui@foxmail.com>
2026-04-12 22:22:48 +0800

    add basic javascript file for add car form

---
+++ addcar.js
@@ -0,0 +1,6 @@
+const addCarForm = document.getElementById("addCarForm");
+
+addCarForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log("Form submitted.");
+});
