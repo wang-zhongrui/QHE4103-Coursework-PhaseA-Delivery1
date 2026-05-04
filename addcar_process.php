9c4ec3e00fefb8d1f5bf01188a0ea7f0c2c08881
Zhongrui Wang <wang.zhongrui@foxmail.com>
2026-05-04 22:35:11 +0800

    implement add car backend with seller foreign key

--- addcar_process.php
+++ addcar_process.php
@@ -1,5 +1,18 @@
 <?php
-session_start();
+require_once 'auth_check.php';
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "AAAA_car_sale";
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$seller_id = $_SESSION['seller_id'];
 
 $colour = $_POST['colour'];
 $model = $_POST['model'];
@@ -7,11 +20,20 @@ $year = $_POST['year'];
 $location = $_POST['location'];
 $price = $_POST['price'];
 
-$image = $_FILES['image']['name'];
+$image_name = $_FILES['image']['name'];
+$tmp_name = $_FILES['image']['tmp_name'];
 
-echo $colour;
-echo "<br>";
-echo $model;
-echo "<br>";
-echo $image;
+$target_path = "uploads/" . $image_name;
+
+move_uploaded_file($tmp_name, $target_path);
+
+$sql = "INSERT INTO cars (seller_id, colour, model, year, location, price, image)
+        VALUES ('$seller_id', '$colour', '$model', '$year', '$location', '$price', '$target_path')";
+
+if ($conn->query($sql) === TRUE) {
+    header("Location: addcar.php?success=1");
+    exit();
+} else {
+    echo "Error: " . $conn->error;
+}
 ?>
